c06bb8e5a518f83e2b0152af6e948ccd716a8439
stropitek <kostro.d@gmail.com>
2017-05-26 22:20:33 +0200

    refactor: make manager logic easier to understand

--- bin/start.js
+++ bin/start.js
@@ -18,37 +18,35 @@ function updateProcessList() {
 pm2.connect(function () {
     pm2.launchBus(function (err, bus) {
         bus.on('pm2-bridge', function (packet) {
-
+            let isReply;
             const data = packet.data;
 
             if (from[data.messageId]) {
                 // This message is a pm2-bridge reply since the messageId exists
+                isReply = true;
                 data.to = from[data.messageId];
                 delete from[data.messageId];
             } else if (data.to) {
                 // This message is a pm2-bridge message
                 from[data.messageId] = packet.process.name;
+                isReply = false;
             } else {
                 // Do nothing, this message is not destined for pm2-bridge
                 return;
             }
 
             const to = data.to;
-            const receivers = processList.filter(p => to === p.name);
             let prom = Promise.resolve();
-            if (receivers.length !== to.length) {
-                prom = prom.then(updateProcessList);
-            }
 
+            if(!isReply) { // No need to refresh the process list if it is a reply
+                prom = updateProcessList();
+            }
             prom.then(() => {
-                var receivers = processList.filter(p => to === p.name);
-                if (receivers.length === 0) {
-                    if (from[data.messageId]) {
-                        sendError(data, 'Receiver process not found');
-                    }
+                let receiver = processList.find(p => to === p.name);
+                if(!receiver && !isReply) {
+                    sendError(data, 'Receiver process not found');
                 }
-                for (let i = 0; i < receivers.length; i++) {
-                    pm2.sendDataToProcessId(receivers[i].pm2_env.pm_id, {
+                    pm2.sendDataToProcessId(receiver.pm2_env.pm_id, {
                         data: {
                             data: data.data,
                             from: packet.process.name
@@ -59,7 +57,6 @@ pm2.connect(function () {
                         // Not adding this callback makes this script exit...
                         // Send error message to sender
                     });
-                }
             }).catch(() => {
                 // if error send message to sender
             });
